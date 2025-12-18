b8f4baa3a1c5f3147d5039a86f4c8bd3420e3b02
neoisone <rushil1234@gmail.com>
2025-12-18 13:14:14 +0100

    fix(infra): scope terraform apply and allow Cloud Build GKE access

--- terraform/modules/network/variables.tf
+++ terraform/modules/network/variables.tf
@@ -1,15 +1,17 @@
 variable "vpc_name" {
   description = "Name of the VPC"
   type        = string
+  default     = "ob-vpc"
 }
 
 variable "subnet_name" {
   description = "Name of the subnet"
   type        = string
+  default     = "ob-subnet"
 }
 
 variable "region" {
   description = "Region for subnet"
   type        = string
+  default     = "us-central1"
 }
-
